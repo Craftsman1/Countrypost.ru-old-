41af5e2fc375aaaa3b751b968538522c0b7d836b
oster <_oster_@mail.ru>
2012-11-12 17:31:06 +0200

    Alter Clients Table Fix

--- dbscripts/2012-11-08 22-40 client profile.sql
+++ dbscripts/2012-11-08 22-40 client profile.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `clients` ADD `website` VARCHAR( 4096 ) NOT NULL AFTER `client_phone` ,
-ADD `skype` VARCHAR( 255 ) NOT NULL AFTER `website`
+ADD `skype` VARCHAR( 255 ) NOT NULL AFTER `website`;
+
+ALTER TABLE `clients` ADD `about_me` TEXT NOT NULL AFTER `notifications_on`; 
