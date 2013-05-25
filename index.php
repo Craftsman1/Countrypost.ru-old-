82e8cefa71551cd4ca41104d1927b4b2d01b1066
Craftsman <at3@yandex.ru>
2013-05-25 18:09:55 +0400

    Disallowed Key Characters и убрал заглушку IE6

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
-if (stripos($user_agent, 'MSIE 6.0') !== false && stripos($user_agent, 'MSIE 8.0') === false && stripos($user_agent, 'MSIE 7.0') === false) {
-   if (!isset($HTTP_COOKIE_VARS["ie"])) {setcookie("ie", "yes", time()+60*60*24*360);header ("Location: static/ie6/ie6.html");}
-}
+
+
+
+
 /*
 |---------------------------------------------------------------
 | PHP ERROR REPORTING LEVEL
