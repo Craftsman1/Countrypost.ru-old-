30d2fc4eb16b7875df6cf1ca10a24a1e6f00d912
Alexandr <cgs.mode@gmail.com>
2013-06-24 18:02:04 +0400

    Задача #34
    Убрана ссылка ink8386/profile

--- system/application/views/main/elements/clients/profile_header.php
+++ system/application/views/main/elements/clients/profile_header.php
@@ -3,7 +3,7 @@
 	<h2 style='margin-bottom: 10px;text-transform: none;'><?= $client->statistics->fullname ?> (<?= $client->statistics->login ?>)</h2>
     <br/>
 	<ul class='tabs'>
-        <li class='active profile'><div><a class='profile' href='/<?= $client->statistics->login ?>/profile'>Профиль</a></div></li>
+        <li class='active profile'><div><a class='profile' href='/<?= $client->statistics->login ?>'>Профиль</a></div></li>
         <? if( ! empty($this->user) AND
             //($this->user->user_group == 'manager' OR
             $this->user->user_id == $client->client_user) ://) : ?>
