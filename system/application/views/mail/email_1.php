e21c72b193767a5007a009114bfa35581866ecb9
Craftsman <at3@yandex.ru>
2013-07-14 14:21:39 +0400

    Изменил текст в рассылке

--- system/application/views/mail/email_1.php
+++ system/application/views/mail/email_1.php
@@ -3,11 +3,10 @@
 {/unwrap}
 {unwrap}
 Сообщаем Вам, что в заказе №<?=$order_id?> Вы были выбраны исполнителем. Чтобы просмотреть заказ, перейдите по этой ссылке:
-{/unwrap}
-{unwrap}
 <?=$this->config->item('base_url')?>manager/order/<?=$order_id?>
 {/unwrap}
 {unwrap}
-C уважением,
+C уважением,{/unwrap}
+{unwrap}
 Countrypost.ru - лучший сервис покупок за рубежом
 {/unwrap}
