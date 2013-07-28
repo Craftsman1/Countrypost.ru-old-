9cb0f3e1665967b1ca530afdcf40745e7f6c8380
Craftsman <at3@yandex.ru>
2013-07-28 21:33:38 +0400

    добавил соц. кнопки на главной

--- system/application/views/main/elements/div_social.php
+++ system/application/views/main/elements/div_social.php
@@ -1,5 +1,30 @@
 <? if (isset($showSocialBox)) : ?>
-<div style="margin-top:20px;left: 0;display: block;float: right;width: 262px;">
-	
+<div align="center" style="margin-top:20px;left: 0;display: block;float: right;width: 262px;">
+	<span class='st_vkontakte_large' displayText='Vkontakte'></span>
+	<span class='st_odnoklassniki_large' displayText='Odnoklassniki'></span>
+	<span class='st_facebook_large' displayText='Facebook'></span>
+	<span class='st_twitter_large' displayText='Tweet'></span>
+	<span class='st_livejournal_large' displayText='LiveJournal'></span>
+	<span class='st_email_large' displayText='Email'></span>
+
+<table border="1" width="100%" style="border-width: 0px">
+	<tr>
+		<td style="border-style: none; border-width: medium">&nbsp;</td>
+	</tr>
+	<tr>
+		<td style="border-style: none; border-width: medium">&nbsp;</td>
+	</tr>
+	<tr>
+		<td align="center" style="border-style: none; border-width: medium">
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="VJNDBQ95CAC3L">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1">
+			</form>
+		</td>
+	</tr>
+</table>
+
 </div>
 <? endif; ?>
--- system/application/views/main/header.php
+++ system/application/views/main/header.php
@@ -3,9 +3,16 @@
 	<link rel="shortcut icon" href="/static/images/favicon.ico" type="image/x-icon">
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Лучший сервис покупок товаров за рубежом. Посредники в Китае, Америке, Германии и др.</title>
+	<meta property="og:title" content="Лучший сервис покупок за рубежом." />
+	<meta property="og:description" content="Поможем сделать заказа из любой страны мира." />
+	<meta property="og:url" content="http://countrypost.ru" />
+	<meta property="og:image" content="https://countrypost.ru/static/images/logo-soshial.jpg" />
+	<meta name="title" content="Лучший сервис покупок товаров за рубежом. Посредники в Китае, Америке, Германии и др" />
 	<meta name="description" content="Сервис покупок товаров за рубежом. Посредники в США. Посредники Таобао. Посредники в Германии. Отзывы о посредниках. Заказ и доставка товаров из любой страны мира." />
 	<meta name="keywords" content="посредник таобао, посредник в китае, посредник в германии, посредники в сша, посредник taobao, посредник в финляндии, посредник польша, посредник ebay, посредник в турции, посредник в англии, посредник в испании, посредник в италии, посредник в америке, посредник таобао в китае, посредник в японии" />
+	<script type="text/javascript">var switchTo5x=true;</script>
+	<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+	<script type="text/javascript">stLight.options({publisher: "a0ede171-e3c9-4976-bae9-a802c13e505f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
 	<meta name='yandex-verification' content='794c4994f3186105' />
 	<meta name="author" content="">
 	<meta name="w1-verification" content="135670173257" />
