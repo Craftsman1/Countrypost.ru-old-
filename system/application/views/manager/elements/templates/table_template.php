49c9e3caacf437e2d55b4984aad3fcad34f6fd28
Alexandr <cgs.mode@gmail.com>
2013-09-07 14:33:42 +0400

    Удалил лишнее из EMS-template.php и Chinapost-template.php and fix table color

--- system/application/views/manager/elements/templates/table_template.php
+++ system/application/views/manager/elements/templates/table_template.php
@@ -15,7 +15,7 @@
     <? for($i = 1; $i <= $lines; $i ++) : ?>
         <tr height="21" style="height:15.75pt">
         <? for($ii = 0; $ii <= $cols-1; $ii++) : ?>
-            <td style="text-align: center; height: 15.75pt; width: 48pt; font-size: 12.0pt; font-weight: 700; color: black; font-style: normal; text-decoration: none; font-family: Calibri, sans-serif; text-align: general; vertical-align: bottom; white-space: nowrap; border: 1px solid; padding-left: 1px; padding-right: 1px; padding-top: 1px">
+            <td style="text-align: center; height: 15.75pt; width: 48pt; font-size: 12.0pt; font-weight: 700; color: black; font-style: normal; text-decoration: none; font-family: Calibri, sans-serif; text-align: general; vertical-align: bottom; white-space: nowrap; border: 1px solid rgb(215,215,215); padding-left: 1px; padding-right: 1px; padding-top: 1px">
                 <? if (isset($data[$k+($lines*$ii)])) {
                     $res = $data[$k+($lines*$ii)];
                     echo $res[0];
@@ -26,7 +26,7 @@
                 }
                 ?>
                 </td>
-            <td style="text-align: center; height: 15.75pt; width: 48pt; font-size: 12.0pt; font-weight: 700; color: black; font-style: normal; text-decoration: none; font-family: Calibri, sans-serif; text-align: general; vertical-align: bottom; white-space: nowrap; border: 1px solid; padding-left: 1px; padding-right: 1px; padding-top: 1px">
+            <td style="text-align: center; height: 15.75pt; width: 48pt; font-size: 12.0pt; font-weight: 700; color: black; font-style: normal; text-decoration: none; font-family: Calibri, sans-serif; text-align: general; vertical-align: bottom; white-space: nowrap; border: 1px solid rgb(215,215,215); padding-left: 1px; padding-right: 1px; padding-top: 1px">
                 <?
                 if ($showflag) {
                     echo $res[1];
