069f1c632d3d4b57f21b0184a2bcadd39cb959e7
Craftsman1 <at3@yandex.ru>
2013-10-02 22:40:07 +0400

    Убрал на главной кнопку Donation

--- system/application/views/main/elements/div_social.php
+++ system/application/views/main/elements/div_social.php
@@ -14,17 +14,7 @@
 	<tr>
 		<td style="border-style: none; border-width: medium">&nbsp;</td>
 	</tr>
-	<tr>
-		<td align="center" style="border-style: none; border-width: medium">
-			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-			<input type="hidden" name="cmd" value="_s-xclick">
-			<input type="hidden" name="hosted_button_id" value="VJNDBQ95CAC3L">
-			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-			<img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1">
-			</form>
-		</td>
-	</tr>
-</table>
+	</table>
 
 </div>
 <? endif; ?>
