f76fb4ce0c7788e3b307f3c6d35b91e696ccf1e8
Craftsman1 <at3@yandex.ru>
2013-11-06 15:08:54 +0400

    Тарифы на доставку из Кореи

---
+++ system/application/views/manager/elements/templates/delivery/6/EMS-template.php
@@ -0,0 +1,240 @@
+0.5 = 25.700
+1.0 = 32.400
+1.5 = 41.500
+2.0 = 50.700
+2.5 = 54.100
+3.0 = 57.300
+3.5 = 60.800
+4.0 = 64.000
+4.5 = 67.300
+5.0 = 70.600
+5.5 = 74.000
+6.0 = 77.300
+6.5 = 80.600
+7.0 = 83.900
+7.5 = 87.200
+8.0 = 90.600
+8.5 = 93.900
+9.0 = 97.200
+9.5 = 100.500
+10.0 = 103.900
+10.5 = 107.200
+11.0 = 110.500
+11.5 = 113.800
+12.0 = 117.200
+12.5 = 120.500
+13.0 = 123.800
+13.5 = 127.100
+14.0 = 130.400
+14.5 = 133.800
+15.0 = 137.000
+15.5 = 140.400
+16.0 = 143.600
+16.5 = 147.100
+17.0 = 150.300
+17.5 = 153.700
+18.0 = 156.900
+18.5 = 160.400
+19.0 = 163.600
+19.5 = 167.000
+20.0 = 170.200
+20.5 = 173.600
+21.0 = 176.900
+21.5 = 180.300
+22.0 = 183.500
+22.5 = 186.900
+23.0 = 190.200
+23.5 = 193.600
+24.0 = 196.800
+24.5 = 200.200
+25.0 = 203.500
+25.5 = 206.900
+26.0 = 210.100
+26.5 = 213.500
+27.0 = 216.700
+27.5 = 220.200
+28.0 = 223.400
+28.5 = 226.700
+29.0 = 230.000
+29.5 = 233.400
+30.0 = 233.700
+before_table_text = "<p><font size="4"><b>EMS (Срок доставки: 7-10 дней)</font></b></p>"
+after_table_text = ""
+currency_table_text = "KRW"
+cols = 3
+width = 600
+
+{country} = 98
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 35
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 98
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 75
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 227
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 65
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 72
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 92
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 125
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 155
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 160
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 172
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 239
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 240
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 3
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 18
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 8
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 237
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 4
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 10
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 178
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 7
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 33
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 88
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 87
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 83
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 86
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 114
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 184
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 99
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 198
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 219
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 158
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 168
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
+
+{country} = 29
+{first_kg} = 0
+{second_kg} = 0
+{extra_rate} = 0
