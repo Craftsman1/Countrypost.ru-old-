b2b00255fe718638482ca3a6c8d7d87859f7a4d8
Craftsman1 <at3@yandex.ru>
2013-11-15 11:00:11 +0400

    Удалил лишние фи лы

---
+++ index1.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>Новая страница 1</title>
+</head>
+
+<body>
+
+Уважаемы клиенты.
+На сайте проводятся технические работы. 
+Все оплаченные заказы выполняются в обычном режиме.
+По всем вопросам вы можете написать на почту:
+info@countrypost.ru
+skype: country_post
+
+Приносим извинения за предоставленные неудобства.
+countrypost.ru</body>
+
+</html>
