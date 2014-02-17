39a86782d6621e747aac664bba5d9537a333f05a
Craftsman1 <123456>
2014-02-17 13:37:59 +0400

    Правка в профиле посредника редактирование страницы с тарифами.

--- system/application/views/manager/elements/pricelist.php
+++ system/application/views/manager/elements/pricelist.php
@@ -1,78 +1,118 @@
-<div class="pricelist pricelist_main table dealer_tab" style="display:none;">
-	<div class='angle angle-lt'></div>
-	<div class='angle angle-rt'></div>
-	<div class='angle angle-lb'></div>
-	<div class='angle angle-rb'></div>
-	<div>
-		<span>
-			<b>ЗАКАЗ:</b>
-		</span>
-	</div>
-	<div>
-		<span>
-			Комиссия за обычный заказ:
-		</span>
-		<span>
-			<b><?= $manager->order_tax ?>%</b> (от общей стоимости товаров и местной доставки)
-		</span>
-	</div>
-	<div>
-		<span>
-			Комиссия за самостоятельный заказ:
-		</span>
-		<span>
-			<b><?= $manager->order_mail_forwarding_tax ?> <?= $manager->statistics->currency ?></b>  (Mail Forwarding)
-		</span>
-	</div>
-	<div>
-		<span>
-			Минимальная комиссия за заказ:
-		</span>
-		<span>
-			<b><?= $manager->min_order_tax ?> <?= $manager->statistics->currency ?></b>
-		</span>
-	</div>
-	<br>
-	<br>
-	<div>
-		<span>
-			<b>ДОПОЛНИТЕЛЬНЫЕ УСЛУГИ:</b>
-		</span>
-	</div>
-	<div>
-		<span>
-			Объединение посылок (консолидация):
-		</span>
-		<span>
-			<b><?= $manager->join_tax ?> <?= $manager->statistics->currency ?></b>
-		</span>
-	</div>
-	<div>
-		<span>
-			Комиссия за одно фото:
-		</span>
-		<span>
-			<b><?= $manager->foto_tax ?> <?= $manager->statistics->currency ?></b>
-		</span>
-	</div>
-	<div style="margin: 0;">
-		<span>
-			Оформление страховки:
-		</span>
-		<span>
-			<b><?= $manager->insurance_tax ?>%</b>
-		</span>
-	</div>
-</div>
-<div class="pricelist dealer_tab" style="display:none;">
-	<h3>Подробное описание</h3>
-	<div class="table">
+<form action="/manager/savePricelist" id="pricelistForm" method="POST">
+	<div class="pricelist pricelist_main table dealer_tab" style="display:none;">
 		<div class='angle angle-lt'></div>
 		<div class='angle angle-rt'></div>
 		<div class='angle angle-lb'></div>
 		<div class='angle angle-rb'></div>
-		<p>
-			<?= empty($manager->pricelist_description) ? 'Нет описания.' : html_entity_decode($manager->pricelist_description) ?>
-		</p>
+		<div>
+			<span>
+				<b>ЗАКАЗ:</b>
+			</span>
+		</div>
+		<div>
+			<span>
+				Комиссия за обычный заказ:
+			</span>
+			<span>
+				<input class="textbox" maxlength="6" type='text' id='order_tax' name="order_tax" value="<?=
+				$manager->order_tax ?>" /> % (от общей стоимости товаров и местной доставки)
+			</span>
+		</div>
+		<div>
+			<span>
+				Комиссия за самостоятельный заказ:
+			</span>
+			<span>
+				<input class="textbox" maxlength="11" type='text' id='mf_tax' name="mf_tax" value="<?=
+				$manager->order_mail_forwarding_tax ?>" /> <?= $manager->statistics->currency ?> (Mail Forwarding)
+			</span>
+		</div>
+		<div>
+			<span>
+				Минимальная комиссия за заказ:
+			</span>
+			<span>
+				<input class="textbox" maxlength="4" type='text' id='min_order_tax' name="min_order_tax" value="<?=
+				$manager->min_order_tax ?>" /> <?= $manager->statistics->currency ?>
+			</span>
+		</div>
+		<br>
+		<div>
+			<span>
+				<b>ДОПОЛНИТЕЛЬНЫЕ УСЛУГИ:</b>
+			</span>
+		</div>
+		<div>
+			<span>
+				Объединение посылок (консолидация):
+			</span>
+			<span>
+				<input class="textbox" maxlength="11" type='text' id='join_tax' name="join_tax" value="<?=
+				$manager->join_tax ?>" /> <?= $manager->statistics->currency ?>
+			</span>
+		</div>
+		<div>
+			<span>
+				Комиссия за одно фото:
+			</span>
+			<span>
+				<input class="textbox" maxlength="11" type='text' id='foto_tax' name="foto_tax" value="<?=
+				$manager->foto_tax ?>" /> <?= $manager->statistics->currency ?>
+			</span>
+		</div>
+		<div>
+			<span>
+				Оформление страховки:
+			</span>
+			<span>
+				<input class="textbox" maxlength="3" type='text' id='insurance_tax' name="insurance_tax" value="<?=
+				$manager->insurance_tax ?>" /> %
+			</span>
+		</div>
 	</div>
-</div>
+	<div class="pricelist dealer_tab" style="display:none;">
+		<h3>Подробное описание</h3>
+		<div class="table">
+			<div class='angle angle-lt'></div>
+			<div class='angle angle-rt'></div>
+			<div class='angle angle-lb'></div>
+			<div class='angle angle-rb'></div>
+			<textarea maxlength="65535" id='pricelist_message' name="pricelist_message"><?= empty
+			($manager->pricelist_description) ?	''	: html_entity_decode($manager->pricelist_description) ?></textarea>
+		</div>
+		<br>
+		<div class="submit floatleft">
+			<div>
+				<input type="submit" value="Сохранить">
+			</div>
+		</div>
+		<img class="float" id="pricelistProgress" style="display:none;margin:0px;margin-top: -2px;margin-left: 8px;"
+			 src="/static/images/lightbox-ico-loading.gif"/>
+	</div>
+</form>
+<script>
+	$(function() {
+		$('#pricelistForm').ajaxForm({
+			target: '/manager/savePricelist',
+			type: 'POST',
+			dataType: 'html',
+			iframe: true,
+			beforeSubmit: function(formData, jqForm, options)
+			{
+				$("#pricelistProgress").show();
+			},
+			success: function(response)
+			{
+				$("#pricelistProgress").hide();
+				success('top', 'Тарифы успешно сохранены!');
+			},
+			error: function(response)
+			{
+				$("#pricelistProgress").hide();
+				error('top', 'Заполните все поля и сохраните еще раз.');
+			}
+		});
+	});
+
+	<?= editor('pricelist_message', 200, 920, 'PackageComment') ?>
+</script>
